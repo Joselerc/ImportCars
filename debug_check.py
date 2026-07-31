abdf640ac59c8f2755dc2f1608db0f5cb2de03b4
Joselerc <jrodriguezcarr2@gmail.com>
2026-07-31 22:09:09 +0200

    security: remove browser sessions and debug artifacts

--- debug_check.py
+++
@@ -1,6 +0,0 @@
-with open('src/import_cars/scrapers/mobile_de.py', 'rb') as f:
-    lines = f.readlines()
-    for i in range(245, 250):
-        if i < len(lines):
-            line = lines[i]
-            print(f'Line {i+1}: {line}')
